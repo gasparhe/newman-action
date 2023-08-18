975fee940605513ef3585977daec4d8a5fbede42
gaspar <gasparhe@gmail.com>
2023-08-18 19:19:33 +0200

    testing in other way

--- index.js
+++ index.js
@@ -1,101 +1,39 @@
 const core = require('@actions/core')
-const newman = require('newman')
+const newman = require('newman');
 
-init()
+init();
 
-async function init () {
+async function init() {
   try {
-    const required = { required: true }
-    const idRegex = /^[0-9]{7,}-\w{8}-\w{4}-\w{4}-\w{4}-\w{12}$/
-
+    const apiBase = 'https://api.getpostman.com';
+    const args = process.argv.slice(2);
     const options = {
-      apiKey: get('apiKey'),
-      collection: get('collection', required),
-      apiBase: get('apiBase', required),
-      environment: get('environment'),
-      envVar: safeParse(get('envVar')),
-      globals: get('globals'),
-      globalVar: safeParse(get('globalVar')),
-      iterationCount: num(get('iterationCount')),
-      iterationData: get('iterationData'),
-      folder: safeParse(get('folder')),
-      workingDir: get('workingDir'),
-      insecureFileRead: safeParse(get('insecureFileRead')),
-      timeout: num(get('timeout')),
-      timeoutRequest: num(get('timeoutRequest')),
-      timeoutScript: num(get('timeoutScript')),
-      delayRequest: num(get('delayRequest')),
-      ignoreRedirects: safeParse(get('ignoreRedirects')),
-      insecure: safeParse(get('insecure')),
-      bail: safeParse(get('bail')),
-      suppressExitCode: safeParse(get('suppressExitCode')),
-      reporters: safeParse(get('reporters')),
-      reporter: safeParse(get('reporter')),
-      color: get('color'),
-      sslClientCert: get('sslClientCert'),
-      sslClientKey: get('sslClientKey'),
-      sslClientPassphrase: get('sslClientPassphrase'),
-      sslClientCertList: safeParse(get('sslClientCertList')),
-      sslExtraCaCerts: get('sslExtraCaCerts'),
-      requestAgents: safeParse(get('requestAgents')),
-      cookieJar: get('cookieJar')
-    }
+      collection: args[0],
+      environment: args[1],
+      reporters: 'cli',
+    };
 
-    
-    
-    const filteredOptions = removeEmpty(options)
-    runNewman(filteredOptions)
+    options.collection = `${apiBase}/collections/${options.collection}?apikey=${args[2]}`;
+    options.bail = true;
+    options.environment = `${apiBase}/environments/${options.environment}?apikey=${args[2]}`;
+
+    runNewman(options);
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 }
 
-function get (key, opts) {
-  const val = core.getInput(key, opts)
-  return val !== '' ? val : null
-}
-
-function safeParse (obj) {
-  if (obj) {
-    try {
-      return JSON.parse(obj)
-    } catch (e) {
-      core.warning('Bad object passed in config!')
-    }
-  }
-
-  return null
-}
-
-function num (i) {
-  if (i) {
-    return Number(i)
-  }
-
-  return i
-}
-
-function removeEmpty (obj) {
-  return Object.entries(obj)
-    .filter(([_, v]) => v != null)
-    .reduce((acc, [k, v]) => ({ ...acc, [k]: v }), {})
-}
-
-function runNewman (options) {
-  core.warning(options);
-  core.warning(options.environment);
-  core.warning(options.collection);
-  
-  options.collection = `${apiBase}/collections/${options.collection}?apikey=${options.apiKey}`
-  options.environment = `${apiBase}/environments/${options.environment}?apikey=${options.apiKey}`
-  
-  newman.run(options, (err) => {
-    if (err) {
-      core.setFailed('Newman run failed!! ' + (err || ''))
-    }
-  }).on('done', (err, summary) => {
-    if (!options.suppressExitCode && (err || summary.run.failures.length)) {
-      core.setFailed('Newman run failed! ' + (err || ''))
-    }
-  })
+function runNewman(options) {
+  console.log(options);
+  newman
+    .run(options, (err) => {
+      if (err) {
+        core.setFailed('Newman run error!! ' + (err || ''), err);
+      }
+    })
+    .on('done', (err, summary) => {
+      if (!options.suppressExitCode && (err || summary.run.failures.length)) {
+        core.setFailed('Newman run failed! ' + (err || ''), summary?.run?.failures);
+      }
+    });
 }
